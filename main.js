5b620634f9462e3e25da1fe82b7967c940daf44c
¨ivan <gonzalezivanuriel@gmail.com>
2023-07-31 22:20:26 -0300

    PreEntrega1-JS

---
+++ main.js
@@ -0,0 +1,43 @@
+alert(`Bienvenid@ a Apple World!!!
+
+Somos presente creando futuro. Presione aceptar, todo lo nuevo y lo que esta por venir a su alcance.`);
+
+const nombre = prompt("Ingrese su nombre.");
+
+if(nombre != ""){
+    alert(`Hola ${nombre} estamos felices de que estes aqui con nosotros.`);
+}else{
+    alert(`Ingrese un nombre valido`);
+}
+
+alert("lo invitamos a nuestra tienda para elejigir lo ultimo en tecnologia");
+
+
+
+/* let producto = iphone14;
+let producto = iphone14pro;
+let producto = iphone14promax */
+
+parseInt(prompt("Ingrese 1 para seleccionar iphone14, 2 para iphone14pro o 3 para iphone14promax"))
+
+/* let producto = iphone14;
+let producto = iphone14pro;
+let producto = iphone14promax */
+
+if (producto = 1){
+    alert(`Usted ha seleccionado ${iphone14}`);
+    }else if(producto = 2) {
+    alert(`Usted ha seleccionado ${iphone14pro}`);
+    }else{
+    alert(`Usted ha seleccionado ${iphone14promax}`);
+    }
+
+/* let personal = "Pedro"
+
+    for (let asesor = 1; asesor = 2; asesor ++){
+        let asesor = prompt ("Para continuar ingrese 1 y se le asignara un asesor comercial para finalizar la compra o 2 para finalizarla de manera personal");
+
+        alert(`Hola ${nombre} su asesor es ${personal}`);
+        alert("Lo sentimos, ya no esta disponible esta funcion. Gracias por elegirnos, lo invitamos a que se comunique al numero que aparece en nustro sitio para poder continuar con su compra")
+        alert("Ingrese un numero valido")
+    }  */
